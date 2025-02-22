9b5b740d41c32ae6a75d3115d96a9bac21eee193
Vaishnavi Thorat <vaishnavit582@gmail.com>
2025-02-22 22:37:46 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const taskRoutes = require("./routers/taskRoute");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 connectDB();
 
